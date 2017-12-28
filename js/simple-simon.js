1bb88000a6ed7aa50d951c0428d73013dcae8b5a
brandongossen <brandongossen@yahoo.com>
2017-12-28 06:37:51 -0600

    Create functions for fade in and out
    -Create functionality that allows the sequence of rectangles to be chosen at random (randomNumber function).
    
    -Create a lightUp function that fades the randomly chosen rectangle to fade in and out.
    
    -Call the lightUp function in the randomNumber function to view the randomly chosen rectangle's color fade in and out.

--- js/simple-simon.js
+++ js/simple-simon.js
@@ -1,25 +1,70 @@
 (function () {
     "use strict";
+    $(document).ready(function () {
 
-    var red = "red";
-    var yellow = "yellow";
-    var green = "green";
-    var blue = "blue";
 
-    var clickColor = {
-        colorClicker: function (color) {
-            console.log(color);
-        }
-    };
+    var colors = [$("#red-box"), $("#yellow-box"), $("#green-box"), $("#blue-box")];
 
-    $("#red-box").click(function() {
-        clickColor.colorClicker(yellow)});
-    $("#yellow-box").click(function() {
-        clickColor.colorClicker(yellow)});
-    $("#green-box").click(function() {
-        clickColor.colorClicker(green)});
-    $("#blue-box").click(function() {
-        clickColor.colorClicker(blue)});
+    var gameArray = [];
 
+    $("#start").click(randomNumber);
+
+    function randomNumber() {
+        var randomColor = Math.floor(Math.random() * (4));
+        gameArray.push(randomColor);
+        lightUp();
+    }
+
+
+    //set interval
+
+    function lightUp() {
+        console.log(gameArray);
+        var count = 0;
+        var max = gameArray.length;
+        var interval = 1000;
+        var intervalID = setInterval(function () {
+            if (count === max) {
+                clearInterval(intervalID);
+            } else {
+                console.log(colors[gameArray[count]]);
+                colors[gameArray[count]].addClass("light");
+                setTimeout(function () {
+                    $(".box").removeClass("light");
+                }, 1000);
+                colors[gameArray[count]]
+                    .animate({
+                        opacity: 1,
+                    }, 500)
+                    .animate({
+                        opacity: .35,
+                    }, 500);
+                count++;
+            }
+        interval});
+    }
+
+    // console.log($(this).attr("data"))
+    //this console.log is giving the data value from the html of each colored box
+
+    // var red = "red";
+    // var yellow = "yellow";
+    // var green = "green";
+    // var blue = "blue";
+    //
+    // var clickColor = {
+    //     colorClicker: function (color) {
+    //         console.log(color);
+    //     }
+    // };
+
+
+    // $("#yellow-box").click(function() {
+    //     clickColor.colorClicker(yellow)});
+    // $("#green-box").click(function() {
+    //     clickColor.colorClicker(green)});
+    // $("#blue-box").click(function() {
+    //     clickColor.colorClicker(blue)});
+    });
 
 })();
